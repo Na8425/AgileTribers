99cd031d498e7264aab53ad4550bdf990ecabcaa
Na8425 <nabeelahamed105@gmail.com>
2025-05-28 12:54:55 +0530

    Add files via upload

---
+++ TASK 1/TASK 1.py
@@ -0,0 +1,83 @@
+print("Hello, World!")#task1
+print("Nabeel Ahamed V K")#task2
+print("21")#task3
+
+#task 4
+
+Name = "Nabeel Ahamed V K"   #sting
+Age = 21                     #integer
+height = 5.9                 #float
+is_student = True            #boolean
+
+print("Name :",Name)
+print("Age :",Age)
+print("Height :",height)
+print("IS_STUDENT :",is_student)
+
+#task 5
+
+print("Name :"+Name +" Age :"+str(Age))
+
+#Task 6
+
+My_name = "Nabeel Ahamed V K"
+My_age = "21"
+print("Name :",My_name)
+print("Age :",My_age)
+
+#Task 7
+
+a=10
+b=3
+
+print("addition :",a+b)
+print("substraction :",a-b)
+print("Multiplication :",a*b)
+print("Division :",a/b)
+
+#Task 8
+
+for i in range (1,5):
+    print("*"*i)
+
+
+#Task 9
+
+Name = "Nabeel Ahamed V K"
+Age = 21
+isStudent = True 
+
+print("Name :",Name)
+print("Age :",Age)
+print("IS_STUDENT :",isStudent)
+
+
+
+Name = "Nihal"
+Age +=1
+isStudent = False 
+
+
+print("Name :",Name)
+print("Age :",Age)
+print("IS_STUDENT :",isStudent)
+
+#task 10
+
+Name = "Nabeel Ahamed V K"  
+Age = 21 
+height = 5.9 
+Weight = 74
+Gender = "Male"
+Degree = "B- TECH"
+from datetime import date
+DOB = date(2004, 4, 5)
+
+
+print("Name :",Name)
+print("Age :",Age)
+print("Height :",height)
+print("Weight :",Weight)
+print("Gender :",Gender)
+print("Date of Birth :",DOB)
+print("Degree :",Degree)
